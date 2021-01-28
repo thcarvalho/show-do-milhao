0a2ac1ced2e21533bed10a82f4f3ad33a4e15b7c
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2021-01-27 22:41:05 -0300

    add routing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import db from '../../db.json';
@@ -32,13 +33,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" type="image/x-icon" href="/public/favicon.ico" />
         <title>Show do Milhão</title>
+        <link id="favicon" rel="icon" type="image/x-icon" href="https://i.imgur.com/xUIBzpe.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../../db.json';
 import Footer from '../components/Footer';
 import GitHubCorner from '../components/GitHubCorner';
@@ -18,6 +20,14 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const router = useRouter();
+
+  function navigateToQuiz(e) {
+    e.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  }
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -28,6 +38,18 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={navigateToQuiz}>
+              <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Quem é o participante?" />
+              {
+                name && (
+                  <button type="submit" disabled={!name}>
+                    Jogar
+                    {' '}
+                    {name}
+                  </button>
+                )
+              }
+            </form>
           </Widget.Content>
         </Widget>
 
